5a86b42a96277a2164aa1f25fe4d610798d3649c
sebastian-sonne <sebastian@familie-sonne.com>
2024-04-16 11:24:27 -0500

    npm run optimization

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,8 @@
 export default function Header() {
     return (
         <>
-            <h1>Header</h1>
+            <h1 className="bg-red-600 text-blue-600">Header</h1>
+            <p className="h-10 w-full bg-slate-300 p-3 m-2">This is the header</p>
         </>
     );
 }
