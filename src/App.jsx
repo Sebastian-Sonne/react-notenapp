a448b94445fa3b3ba321363f3b459d7e5f7cb311
sebastian-sonne <sebastian@familie-sonne.com>
2024-04-24 08:40:41 -0500

    App refactored

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,12 @@ import * as Notify from './components/Notification.jsx';
  */
 export const App = () => {
 
-    if (typeof (Storage) === 'undefined') return <Notify.NoLocalStorage />;
+    const [students, setStudents] = useState((typeof (Storage) === 'undefined') ? storageModule.loadData('students') : []);
+    const [currentStudent, setCurrentStudent] = useState(null);
+    const [showForm, setShowForm] = useState(false);
+    const [showInfo, setShowInfo] = useState(false);
+    const [notification, setNotification] = useState(''); //setNofitication('') to hide component
 
-    const [students, setStudents] = useState(storageModule.loadData('students'));
     const addStudent = (student) => {
         storageModule.saveStudent(student);
         setStudents(storageModule.loadData('students'));
@@ -24,63 +27,54 @@ export const App = () => {
         setNotification('Schüler Erfolgreich Gelöscht');
     }
 
-    const [currentStudent, setCurrentStudent] = useState(null);
-
-    const [showForm, setShowForm] = useState(false);
     const toggleForm = () => { setShowForm(!showForm) };
-
-    const [showInfo, setShowInfo] = useState(false);
     const toggleInfo = () => { setShowInfo(!showInfo) };
 
-    //setNofitication('') to hide component
-    const [notification, setNotification] = useState('');
-
-    /**
-     * handle body overflow 
-     */
     useEffect(() => {
-        if (showForm || showInfo) {
-            document.body.classList.add('overflow-hidden');
-        } else {
-            document.body.classList.remove('overflow-hidden');
-        }
+        (showForm || showInfo) ? document.body.classList.add('overflow-hidden')
+            : document.body.classList.remove('overflow-hidden');
 
-        return () => {
-            document.body.classList.remove('overflow-hidden');
-        };
+        return () => { document.body.classList.remove('overflow-hidden') };
     }, [showForm, showInfo]);
 
+    if (typeof (Storage) === 'undefined') return <Notify.NoLocalStorage />;
+
     return (
-        <div className={'flex justify-center min-h-screen'}>
-            <div className='flex flex-col max-w-6xl w-full'>
-                <Components.Header />
+        <>
+            {typeof (Storage) === 'undefined' ? (<Notify.NoLocalStorage />) : (
+                <div className={'flex justify-center min-h-screen'}>
+                    <div className='flex flex-col max-w-6xl w-full'>
+                        
+                        <Components.Header />
 
-                <Notify.Notification
-                    notification={notification}
-                    setNotification={setNotification} />
+                        <Notify.Notification
+                            notification={notification}
+                            setNotification={setNotification} />
 
-                <Components.SubHeader
-                    toggleForm={toggleForm} />
+                        <Components.SubHeader
+                            toggleForm={toggleForm} />
 
-                <Components.TableContainer
-                    students={students}
-                    toggleInfo={toggleInfo}
-                    setCurrentStudent={setCurrentStudent} />
+                        <Components.TableContainer
+                            students={students}
+                            toggleInfo={toggleInfo}
+                            setCurrentStudent={setCurrentStudent} />
 
-                <Components.Footer />
+                        <Components.Footer />
 
-                <Components.NewStudentBox
-                    isVisible={showForm}
-                    toggleForm={toggleForm}
-                    addStudent={addStudent} />
+                        <Components.NewStudentBox
+                            isVisible={showForm}
+                            toggleForm={toggleForm}
+                            addStudent={addStudent} />
 
-                <Components.StudentInfoBox
-                    isVisible={showInfo}
-                    toggleInfo={toggleInfo}
-                    student={currentStudent}
-                    deleteStudent={deleteStudent} />
-            </div>
-        </div>
+                        <Components.StudentInfoBox
+                            isVisible={showInfo}
+                            toggleInfo={toggleInfo}
+                            student={currentStudent}
+                            deleteStudent={deleteStudent} />
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
 
